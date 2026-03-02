9889679d2cb2e2b28ed0dde02de0494b25c2e524
Mohamed El-Ashry <mohomed.1433@hotmail.com>
2026-03-02 16:41:26 +0200

    last commit

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -24,7 +24,11 @@ const auth = () => {
         <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
           <div className="flex flex-col items-center gap-2 text-center">
             <h1>Welcome</h1>
-            <h2>Log In to Continue Your Job Journey</h2>
+            {auth.isAuthenticated ? (
+              <h2>{auth.user?.username}</h2>
+            ) : (
+              <h2>Log In to Continue Your Job Journey</h2>
+            )}
           </div>
           <div>
             {isLoading ? (
